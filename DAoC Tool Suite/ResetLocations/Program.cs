cfa90a53dc2fe4d15246864ea82d4a8611388885
Taldren76 <Paldren76>
2023-06-25 11:23:03 -0400

    Minor update to include new appdata folder.

--- DAoC Tool Suite/ResetLocations/Program.cs
+++ DAoC Tool Suite/ResetLocations/Program.cs
@@ -1,11 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-namespace DAoCToolsResetSettings
+namespace DAoCToolSuite.DAoCToolsResetSettings
 {
     public static class Program
     {
         [STAThread]
-        public static void Main()
+        public static void Main(string[] args)
         {
             Console.WriteLine("Restoring Default Settings.");
             string ChimpToolPath = Environment.ExpandEnvironmentVariables(@"%LOCALAPPDATA%\ChimpTool\");
@@ -16,6 +16,7 @@ namespace DAoCToolsResetSettings
             string Tald_Inc = Environment.ExpandEnvironmentVariables(@"%LOCALAPPDATA%\Taldren Inc\");
             string VSx86 = Environment.ExpandEnvironmentVariables(@"%LOCALAPPDATA%\VirtualStore\Program Files(x86)\Taldren, Inc\");
             string VS = Environment.ExpandEnvironmentVariables(@"%LOCALAPPDATA%\VirtualStore\Program Files\Taldren Inc\");
+            string AppData = Environment.ExpandEnvironmentVariables(@"%APPDATA%\Taldren, Inc\DAoC Tool Suite");
 
             DelDirectory(ChimpToolPath);
             DelDirectory(CharacterToolPath);
@@ -25,6 +26,19 @@ namespace DAoCToolsResetSettings
             DelDirectory(Tald_Inc);
             DelDirectory(VSx86);
             DelDirectory(VS);
+
+            if (args.Length == 0)
+                return;
+            var command = args[0];
+            switch (command.ToLower())
+            {
+                case "/all":
+                    DelDirectory(AppData);
+                    break;
+                default:
+                    Console.WriteLine("Invalid command");
+                    break;
+            }
         }
 
         private static void DelDirectory(string dirName)
