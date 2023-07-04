ee2985f67c33ca7fb7ef6eb384c68e76956c63d7
Taldren76 <Paldren76>
2023-07-04 11:49:02 -0400

    Namespace change.

--- DAoC Tool Suite/ResetLocations/Program.cs
+++ DAoC Tool Suite/ResetLocations/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-namespace DAoCToolsResetSettings
+namespace DAoCToolSuite.DAoCToolsResetSettings
 {
     public static class Program
     {
